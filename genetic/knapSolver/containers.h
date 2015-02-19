165c53710435b1b1035d3176f37ea828ba5b7929
Jonathan <livingjigsaw@gmail.com>
2015-02-19 01:14:17 -0600

    Part 1
    Goal of part one was to create a population for a knapsack
    problem, and rank them. Currently this program generates a
    population with random attributes, and sorts them within a
    population object, where the first stored candidate is the
    highest-ranked based on fitness. It prints out the fitnesses
    of the population in order, as well as the top-ranked member
    data.

--- genetic/knapSolver/containers.h
+++ genetic/knapSolver/containers.h
@@ -1,5 +1,7 @@
 #include<set>
 #include<vector>
+#include<cstdlib>	//need rand and srand functions
+#include<ctime>		//time function used to initialize seed in main
 using namespace std;
 
 struct knapItem{
@@ -17,11 +19,10 @@ struct Answer{
 	void reset(){
 		totalCost = 0;
 		totalValue = 0;
-		nodeCount = 0;
 		itemNames.clear();
 	}
 
-	void print(bool nodes){
+	void print(){
 		set<string>::iterator it;
 		cout<< "items in answer:\n";
 		for(it=itemNames.begin();it!=itemNames.end();it++){
@@ -30,9 +31,75 @@ struct Answer{
 		cout << endl;
 		cout << "the cost = " << totalCost << endl; 
 		cout << "the value = " << totalValue << endl <<endl; 
-		if(nodes){
-			cout << "the number of nodes = " << nodeCount << endl <<endl; 	
-		}
 		
 	}
+};
+
+class Candidate{
+	private:
+		//members
+		double fitness;
+		vector<bool> sack;
+	
+	public:
+		//constructors/destructors
+		Candidate(){};	//so the array isn't created for child generation
+		Candidate(int inSize){
+			for(int i=0;i<inSize;i++){
+				sack.insert(sack.end(), rand()%2);
+			}
+		};
+
+		~Candidate(){};
+
+		//methods
+		int get_sackSize(){return sack.size();};
+		int get_item(int index){return sack[index];};
+		double get_fit(){return fitness;};
+
+		void set_fit(double inFit){fitness=inFit;};
+		void print(){
+			for(int i=0;i<sack.size();i++){
+				cout<<sack[i];
+			}
+		};
+		//void becomeChild(){}; //weird, but it will take two parents and crossover self
+};
+
+class Population{
+	private:
+		vector<Candidate*> current;
+	public:
+		Population(){};
+		Population(int inSize){
+			for(int i=0;i<inSize;i++){ 
+				current.insert(current.end(), NULL);
+			}
+		};
+		~Population(){};
+
+		void insertMem(Candidate* inCan){
+			for(vector<Candidate*>::iterator it=current.begin();it<current.end();it++){
+				if( (*it)==NULL || inCan->get_fit()> (*it)->get_fit()){
+					current.insert( it , inCan);
+					delete *current.end();
+					current.pop_back();
+					return;
+				}
+			}
+			delete inCan;
+		};
+
+		void print(){
+			for(int i=0;i<current.size();i++){ 
+				cout << current[i]->get_fit() << endl;
+			}
+		};
+
+		void printMember(int index){
+			current[index]->print();
+		};
+		double getMemberFit(int index){
+			return current[index]->get_fit();
+		};
 };
--- genetic/knapSolver/main.cpp
+++ genetic/knapSolver/main.cpp
@@ -1,7 +1,101 @@
 #include<iostream>
+#include<fstream>
+#include<sstream>
 #include"containers.h"
 using namespace std;
 
-int main(){
+bool parseFile(vector<knapItem*>* outList, int* outCost ){
+	string filename;
+	cout << "Please enter the filename of the input file\n";
+	cin >> filename;
+	ifstream infile(filename.c_str());
+	//cout << "attempted to open file\n";
+	if (infile.is_open())
+	{
+		string currentLine;
+		stringstream ss;
+		getline(infile, currentLine);
+		ss << currentLine;
+		ss >> *outCost;
 	
+		//cout << "about to get lines\n";
+		while(getline(infile, currentLine)){
+			//cout << currentLine;
+			ss << currentLine;
+			char parse;
+			string tempName="";
+			int tempCost;
+			int tempValue;
+			ss >> parse;
+			while(parse!=','){
+				//cout << "parse = " << parse << endl;
+				tempName += parse;
+				ss >> parse;
+			}
+			ss >> tempCost;
+			ss >> parse;
+			ss >> tempValue;
+			knapItem* tempItem = new knapItem();
+			tempItem->name=tempName;
+			tempItem->cost=tempCost;
+			tempItem->value=tempValue;
+			outList->insert(outList->end(), tempItem);		
+		//	cout << "looping\n";
+		}
+		//cout << "out of parsing\n";
+		infile.close();
+
+		return true;
+	}
+	else{
+		return false; 
+	}
+}
+double fitEval(Candidate inSoln, vector<knapItem*> inList, int costLimit){
+	Answer temp;
+	temp.reset();
+	for(int i=0;i<inSoln.get_sackSize();i++){
+		if(inSoln.get_item(i)){
+			temp.totalCost+=inList[i]->cost;
+			temp.totalValue+=inList[i]->value;
+			temp.itemNames.insert(inList[i]->name);
+		}
+	}
+	if(temp.totalCost>costLimit){
+		return 0.0;
+	}
+	else{
+		double fitness=temp.totalValue;
+		return fitness;		//may do more math to fitness, not decided yet
+	}
+}
+
+int main(){
+	int costLimit;
+	int fitCount;
+	vector<knapItem*> itemList;
+
+	bool success=parseFile(&itemList, &costLimit);
+	while(!success){ 	
+		cout << "Sorry, a file by that name was not found\n";
+		success=parseFile(&itemList, &costLimit);		
+	}
+		
+	srand(time(NULL));	//initialize the seed for randomness
+	cout << "what size do you want the population to be?\n";
+	int popSize;
+	cin >> popSize;
+	Population genitor(popSize);
+	for(int i=0;i<popSize;i++){
+		Candidate* soln= new Candidate(itemList.size());
+		double temp = fitEval(*soln, itemList, costLimit);
+		soln->set_fit(temp);
+		genitor.insertMem(soln);
+	}
+	cout << "population fitnesses:\n";
+	genitor.print();
+
+
+	cout << "top random candidate = "; genitor.printMember(0); cout << endl;
+	//cout << "Fitness of top candidate = " <<genitor.getMemberFit(0)<<endl;
 }
